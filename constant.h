8b0cbb36e1901e5098c54dca1f3b97f76068888b
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-29 22:03:30 -0700

    cpu states

---
+++ constant.h
@@ -0,0 +1,17 @@
+#ifndef PATH_H
+#define PATH_H
+
+enum CPUStates {
+    S_USER = 1,
+    S_NICE,
+    S_SYSTEM,
+    S_IDLE,
+    S_IOWAIT,
+    S_IRQ,
+    S_SOFTIRQ,
+    S_STEAL,
+    S_GUEST,
+    S_GUEST_NICE
+};
+
+#endif
