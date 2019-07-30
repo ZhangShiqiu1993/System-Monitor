f33f8a6c1af5fc006fa47f529950bd3277da24a0
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-29 22:13:04 -0700

    convertToTime

--- util.h
+++ util.h
@@ -7,3 +7,13 @@ public:
     static std::string getProgressBar(std::string percent);
     static std::ifstream getStream(std::string path);
 };
+
+std::string Util::convertToTime(long int input_seconds)
+{
+    long minutes = input_seconds / 60;
+    long hours = minutes / 60;
+    long seconds = int(input_seconds % 60);
+    minutes = int(minutes % 60);
+    std::string result = std::to_string(hours) + ":" + std::to_string(minutes) + ":" + std::to_string(seconds);
+    return result;
+}
