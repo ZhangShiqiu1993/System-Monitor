5960a8482344d2dfa100e506f2cad1de76673589
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-29 22:10:29 -0700

    declare util class

---
+++ util.h
@@ -0,0 +1,9 @@
+#include <string>
+#include <fstream>
+
+class Util {
+public:
+    static std::string convertToTime(long int input_seconds);
+    static std::string getProgressBar(std::string percent);
+    static std::ifstream getStream(std::string path);
+};
