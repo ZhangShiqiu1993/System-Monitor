0008d50279b91625e13f056a500c4a2703eede06
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-30 22:10:07 -0700

    getProcUpTime

--- ProcessParser.h
+++ ProcessParser.h
@@ -88,3 +88,16 @@ string ProcessParser::getCpuPercent(string pid)
     result = 100 * ((total_time / freq) / seconds);
     return to_string(result);
 }
+
+string ProcessParser::getProcUpTime(string pid) {
+    string line;
+    string value;
+    float result;
+    ifstream stream = Util::getStream((Path::basePath() + pid + "/" + Path::statPath()));
+    getline(stream, line);
+    string str = line;
+    istringstream buf(str);
+    istream_iterator<string> beg(buf), end;
+    vector<string> values(beg, end);
+    return to_string(float(stof(values[13]) / sysconf(_SC_CLK_TCK)));
+}
