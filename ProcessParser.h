d44e01054818b45aa340b6a55c6370f37fa4b407
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-30 20:27:39 -0700

    using std namespace

--- ProcessParser.h
+++ ProcessParser.h
@@ -19,38 +19,42 @@
 #include "constant.h"
 #include "util.h"
 
+using std::ifstream;
+using std::string;
+using std::vector;
+
 class ProcessParser
 {
 public:
-    static std::string getCmd(std::string pid);
-    static std::vector<std::string> getPidList();
-    static std::string getVmSize(std::string pid);
-    static std::string getCpuPercent(std::string pid);
+    static string getCmd(string pid);
+    static vector<string> getPidList();
+    static string getVmSize(string pid);
+    static string getCpuPercent(string pid);
     static long int getSysUpTime();
-    static std::string getProcUpTime(std::string pid);
-    static std::string getProcUser(std::string pid);
-    static std::vector<std::string> getSysCpuPercent(std::string coreNumber = "");
+    static string getProcUpTime(string pid);
+    static string getProcUser(string pid);
+    static vector<string> getSysCpuPercent(string coreNumber = "");
     static float getSysRamPercent();
-    static std::string getSysKernelVersion();
+    static string getSysKernelVersion();
     static int getTotalThreads();
     static int getTotalNumberOfProcesses();
     static int getNumberOfRunningProcesses();
     static string getOsName();
-    static std::string printCpuStats(std::vector<std::string> values1, std::vector<std::string> values2);
+    static string printCpuStats(vector<string> values1, vector<string> values2);
 };
 
-std::string ProcessParser::getVmSize(std::string pid) {
-    std::string line;
-    std::string name = "VmData";
-    std::string value;
+string ProcessParser::getVmSize(string pid) {
+    string line;
+    string name = "VmData";
+    string value;
 
     float result;
-    std::ifstream stream = Util::getStream((Path::basePath() + pid + Path::statusPath()));
+    ifstream stream = Util::getStream((Path::basePath() + pid + Path::statusPath()));
     while (std::getline(stream, line)) {
         if (line.compare(0, name.size(), name) == 0) {
             std::istringstream buf(line);
             std::istream_iterator<string> beg(buf), end;
-            std::vector<std::string> values(beg, end);
+            vector<string> values(beg, end);
             result = (stof(values[1]) / float(1024 * 1024));
             break;
         }
