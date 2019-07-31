e7ffb7b15064c130aae6fe61bd9e3cbd3c82b13d
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-30 22:35:07 -0700

    getCmd

--- ProcessParser.h
+++ ProcessParser.h
@@ -171,3 +171,11 @@ vector<string> ProcessParser::getPidList() {
     }
     return container;
 }
+
+string ProcessParser::getCmd(string pid)
+{
+    string line;
+    ifstream stream = Util::getStream((Path::basePath() + pid + Path::cmdPath()));
+    getline(stream, line);
+    return line;
+}
