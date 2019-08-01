4eb9335a65601d4937f8f2b9e1fe8e16ad1fefac
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-31 20:15:31 -0700

    process method

--- Process.h
+++ Process.h
@@ -35,4 +35,23 @@ public:
     string getProcess();
 };
 
+void Process::setPid(int pid)
+{
+    this->pid = pid;
+}
+
+string Process::getPid() const
+{
+    return this->pid;
+}
+
+string Process::getProcess()
+{
+    this->mem = ProcessParser::getVmSize(this->pid);
+    this->upTime = ProcessParser::getProcUpTime(this->pid);
+    this->cpu = ProcessParser::getCpuPercent(this->pid);
+
+    return (this->pid + "   " + this->user + "   " + this->mem.substr(0, 5) + "     " + this->cpu.substr(0, 5) + "     " + this->upTime.substr(0, 5) + "    " + this->cmd.substr(0, 30) + "...");
+}
+
 #endif
