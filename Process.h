9c96c29eb60b329d0103b4745d1dba8df5031fec
Shiqiu Zhang <zhangshiqiu1993@gmail.com>
2019-07-31 20:11:59 -0700

    Process

---
+++ Process.h
@@ -0,0 +1,38 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include <string>
+#include "ProcessParser.h"
+using std::string;
+
+class Process {
+private:
+    string pid;
+    string user;
+    string cmd;
+    string cpu;
+    string mem;
+    string upTime;
+
+public:
+    Process(string pid) 
+    {
+        this->pid = pid;
+        this->user = ProcessParser::getProcUser(pid);
+        this->mem = ProcessParser::getVmSize(pid);
+        this->cmd = ProcessParser::getCmd(pid);
+        this->upTime = ProcessParser::getProcUpTime(pid);
+        this->cpu = ProcessParser::getCpuPercent(pid);
+    }
+
+    void setPid(int pid);
+    string getPid() const;
+    string getUser() const;
+    string getCmd() const;
+    int getCpu() const;
+    int getMem() const;
+    string getUpTime() const;
+    string getProcess();
+};
+
+#endif
